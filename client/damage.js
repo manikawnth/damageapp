abfa5a6c3ea8dda68214761b5ace146ba7287c2e
Manikanth <manikawnth@gmail.com>
2017-07-11 16:57:53 -0400

    Add files via upload

---
+++ client/damage.js
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+/*function getDate(){
+	document.getElementById('demo').value= Date();
+}*/
+
+/*<script>
+  $(function() {
+    $('#toggle-one').bootstrapToggle();
+  })
+</script>*/
+
+/*
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+*/
+
+/*$(".dropdown-toggle").dropdown();
+
+
+
+$('.dropdown-menu').on( 'click', 'a', function() {
+  var target = $(this).closest('.dropdown').find('.dropdown-toggle')
+  var text = "Number of partners: ";
+  var selectedVal = $(this).html();
+  var htmlText = text + selectedVal + ' <span class="caret"></span>';
+  target.html(htmlText);
+});
+*/
+
+
+
